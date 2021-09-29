a40343c08132a36a94c37eb8ced29f001a774dc2
andrew <andrewmagdyanis@gmail.com>
2021-09-29 14:16:57 +0200

    animation

--- lib/widgets/auth/auth_app_logo.dart
+++ lib/widgets/auth/auth_app_logo.dart
@@ -1,10 +1,11 @@
+import 'dart:math';
 import 'dart:ui';
 import 'package:flutter/material.dart';
 
 import '../../constants.dart';
 import '../logoIcon.dart';
 
-class AuthAppLogo extends StatelessWidget {
+class AuthAppLogo extends StatefulWidget {
   const AuthAppLogo({
     Key? key,
     required this.w,
@@ -15,40 +16,116 @@ class AuthAppLogo extends StatelessWidget {
   final double h;
 
   @override
-  Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        color: metalicGolden,
-        shape: BoxShape.circle,
-        gradient: RadialGradient(
-          colors: [
-            metalicGolden,
-            metalicGolden,
-            metalicGolden,
-            metalicGolden,
-            metalicGolden,
-            metalicGolden,
-            metalicGolden,
-            metalicGolden,
-            metalicGolden,
-            metalicGolden,
+  _AuthAppLogoState createState() => _AuthAppLogoState();
+}
 
-            metalicGolden.withOpacity(0.2),
-            metalicGolden.withOpacity(0.2),
-            Colors.red,
+class _AuthAppLogoState extends State<AuthAppLogo>
+    with TickerProviderStateMixin {
+  late AnimationController animationController;
 
-          ],
-        ),
-      ),
-      height: w * 0.42,
-      width: w * 0.42,
-      padding: EdgeInsets.all(w * 0.035),
-      // child: Hero(
-      //   tag: 'logo',
-      //   child: LogoAsIcon(
-      //     iconLocation: 'assets/icons/splashSmall.png',
-      //   ),
-      // ),
+  @override
+  void initState() {
+    super.initState();
+    animationController = AnimationController(
+      vsync: this,
+      duration: Duration(milliseconds: 5000),
     );
+    animationController.repeat();
+  }
+
+  @override
+  void dispose() {
+    animationController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedBuilder(
+        animation: animationController,
+        builder: (_, child) {
+          return Container(
+            transformAlignment: Alignment.center,
+            transform: Matrix4.rotationZ(animationController.value * 2 * pi),
+            child: Container(
+              transformAlignment: Alignment.center,
+              transform: Matrix4.rotationY(animationController.value * 4 * pi),
+              child: Container(
+                transformAlignment: Alignment.center,
+                transform:
+                    Matrix4.rotationX(animationController.value  * pi),
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: metalicGolden,
+                    shape: BoxShape.circle,
+                    gradient: RadialGradient(
+                      colors: [
+                        metalicGolden,
+                        metalicGolden,
+                        metalicGolden,
+                        metalicGolden,
+                        metalicGolden,
+                        metalicGolden,
+                        metalicGolden,
+                        metalicGolden,
+                        metalicGolden,
+                        metalicGolden,
+                        metalicGolden.withOpacity(0.2),
+                        metalicGolden.withOpacity(0.2),
+                        Colors.red,
+                      ],
+                    ),
+                  ),
+                  height: widget.w * 0.42,
+                  width: widget.w * 0.42,
+                  padding: EdgeInsets.all(widget.w * 0.035),
+                  // child: Hero(
+                  //   tag: 'logo',
+                  //   child: LogoAsIcon(
+                  //     iconLocation: 'assets/icons/splashSmall.png',
+                  //   ),
+                  // ),
+                ),
+              ),
+            ),
+          );
+
+          return RotationTransition(
+            turns: Tween(begin: 0.0, end: 1.0).animate(animationController),
+            child: Container(
+              decoration: BoxDecoration(
+                color: metalicGolden,
+                shape: BoxShape.circle,
+                gradient: RadialGradient(
+                  colors: [
+                    metalicGolden,
+                    metalicGolden,
+                    metalicGolden,
+                    metalicGolden,
+                    metalicGolden,
+                    metalicGolden,
+                    metalicGolden,
+                    metalicGolden,
+                    metalicGolden,
+                    metalicGolden,
+                    metalicGolden.withOpacity(0.2),
+                    metalicGolden.withOpacity(0.2),
+                    Colors.red,
+                  ],
+                ),
+              ),
+              height: widget.w * 0.42,
+              width: widget.w * 0.42,
+              padding: EdgeInsets.all(widget.w * 0.035),
+              child: Text('>><<'),
+              // child: Hero(
+              //   tag: 'logo',
+              //   child: LogoAsIcon(
+              //     iconLocation: 'assets/icons/splashSmall.png',
+              //   ),
+              // ),
+            ),
+          );
+        });
   }
 }
